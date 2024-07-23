32412cfeef16bc93190a3313ec3ab1f3e77a78c7
SREENA M <138745389+sreena13@users.noreply.github.com>
2024-07-23 22:51:21 +0530

    Create String Intro.java

---
+++ 23.07.24/String Intro.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        System.out.println(A.length()+B.length());
+        int ord=A.compareTo(B);
+        if(ord>0){
+            System.out.println("Yes");
+        }
+        System.out.println("No");
+        System.out.println(A.substring(0,1).toUpperCase()+ A.substring(1,A.length())+" "+B.substring(0,1).toUpperCase()+B.substring(1,B.length()));    
+    }
+}
+
+
+
